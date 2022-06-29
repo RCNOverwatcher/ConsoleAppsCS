dd645978216f69354260533580822bd4a7a4c642
Jake <Jake@LAPTOP-SKD1M16H>
2022-06-29 09:44:03 +0100

    Add project files.

---
+++ ConsoleAppCS/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+
+Random rand = new Random();
+Console.WriteLine("Hello, World!");
+Thread.Sleep(5000);
+Console.WriteLine("Tell me your name!");
+String name = Console.ReadLine();
+Console.WriteLine("Nice to meet you " + name + " lmao");
+
+Console.ReadLine();
